e469e2fa9f485963de19f9c38c5377f37d5d2410
Zaim211 <yousseftitazaim@gmail.com>
2025-09-09 16:30:00 +0100

    update dashboard

--- smart-diagrams/src/utils/axiosInstance.js
+++ smart-diagrams/src/utils/axiosInstance.js
@@ -1,47 +1,15 @@
-// import axios from "axios";
-
-// const axiosInstance = axios.create({
-//     baseURL: process.env.NEXT_PUBLIC_BASE_URL,
-//     withCredentials: true,
-
-// });
-
-// axiosInstance.interceptors.request.use((config) => {
-//     const token = sessionStorage.getItem("token");
-//     if (token) {
-//         config.headers["token"] = `Bearer ${token}`;
-//     }
-//     return config;
-// });
-
-// export default axiosInstance;
-
-
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
-  withCredentials: true,
+    baseURL: process.env.NEXT_PUBLIC_BASE_URL,
 });
-
 
 axiosInstance.interceptors.request.use((config) => {
-  const token = sessionStorage.getItem("token");
-  if (token && !config.headers["Authorization"]) {
-    config.headers["Authorization"] = `Bearer ${token}`;
-  }
-  return config;
-});
-
-axiosInstance.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      sessionStorage.removeItem("token");
-      window.location.href = '/';
+    const token = sessionStorage.getItem("token");
+    if (token) {
+        config.headers["token"] = `Bearer ${token}`;
     }
-    return Promise.reject(error);
-  }
-);
+    return config;
+});
 
 export default axiosInstance;
