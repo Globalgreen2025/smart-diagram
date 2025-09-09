d57d4fb52f705c64db8dca6ea705ad4334c767ef
Zaim211 <yousseftitazaim@gmail.com>
2025-09-09 21:17:08 +0100

    update

--- smart-diagrams/src/app/login/page.js
+++ smart-diagrams/src/app/login/page.js
@@ -14,10 +14,9 @@ function Page() {
     //     window.location.href = authUrl;
     //   };
     const handleLogin = () => {
-        // Point directly to your Express backend callback
         const redirectUri = 'https://smart-diagram.onrender.com/api/linkedin/callback';
         
-        const authUrl = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${process.env.NEXT_PUBLIC_CLIENT_ID}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=openid%20profile%20email`;
+        const authUrl = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${process.env.NEXT_PUBLIC_LINKEDIN_CLIENT_ID}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=openid%20profile%20email`;
         
         window.location.href = authUrl;
     };
